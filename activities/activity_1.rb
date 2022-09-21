d56f50276a1edaeefb2b8d0213aac12f5d4ef87a
Https-ErwinPogi <100741046+Https-ErwinPogi@users.noreply.github.com>
2022-09-22 00:04:40 +0800

    Update activity_1.rb
    Activity: 1 Array-based age calculation 

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,16 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+def arrAges(values)
+  array = []
+  values.each do |age|
+    if age >= 18
+      array << age
+    end
+  end
+return array = array.sum / array.size.to_f
+end
+
+
+p arrAges ([12,18,19,20])
