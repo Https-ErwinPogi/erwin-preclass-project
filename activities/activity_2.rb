df6ddee9d95c3c408ce506aff94ebb4b2b43d89d
Https-ErwinPogi <100741046+Https-ErwinPogi@users.noreply.github.com>
2022-09-22 21:23:02 +0800

    Update activity_2.rb
    Activity: 2 Print the full name using hash.

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,18 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def find_name(name_value)
+  # Create a Hash of names
+  names = { first_name: "Erwin ", middle_name: "Pariente ", last_name: "Lomibao" }
+
+
+  names.each do |details, name|
+    if details == name_value
+
+      return name
+    end
+  end
+end
+
+print find_name(:first_name) + find_name(:middle_name)  + find_name(:last_name)
